1032760712e1e8446bae17c5e9d090208a944dd7
TheoLendler <tl42546@srcs.org>
2021-08-30 17:39:53 -0500

    Completed Activity 3

--- Crab.java
+++ Crab.java
@@ -30,12 +30,25 @@ public class Crab extends Actor
     // Checks for user key presses so user can turn the Crab
     private void checkKeyPress( )
     {
-    
+      if(Greenfoot.isKeyDown("right"))
+      {
+          turn(4);
+      }
+     
+      if(Greenfoot.isKeyDown("left"))
+      {
+          turn(-4);
+      }
+ 
     }
     // Checks for collisions with other objects  
     private void onCollision( )
     {
-      
+      if(isTouching(Worm.class))
+      {
+          removeTouching(Worm.class);
+          Greenfoot.playSound("slurp.wav");
+      }
     }
 }
 
